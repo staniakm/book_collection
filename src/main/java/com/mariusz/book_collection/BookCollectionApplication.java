1f84e59d6b0b199f1c45e06d94d1e85904138446
Mariusz <!Shadow83>
2018-07-16 21:20:29 +0200

    add applicaton files

---
+++ src/main/java/com/mariusz/book_collection/BookCollectionApplication.java
@@ -0,0 +1,12 @@
+package com.mariusz.book_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCollectionApplication.class, args);
+    }
+}
