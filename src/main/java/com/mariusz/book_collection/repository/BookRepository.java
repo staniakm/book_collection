26ec8c125fb4e2982a8a396f6b24610188b5cd99
Mariusz <!Shadow83>
2018-07-17 18:24:23 +0200

    add book repository

---
+++ src/main/java/com/mariusz/book_collection/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.mariusz.book_collection.repository;
+
+import com.mariusz.book_collection.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findById(Long id);
+    Optional<Book> findByIsbn(String isbn);
+}
