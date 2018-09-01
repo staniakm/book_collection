8013f7c45c9885e10efd9317d3cee21289cabeed
Mariusz <mariusz.staniak@gmail.com>
2018-09-01 09:26:41 +0200

    add book equals method

--- src/main/java/com/mariusz/book_collection/entity/Book.java
+++ src/main/java/com/mariusz/book_collection/entity/Book.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -23,4 +24,17 @@ public class Book {
     private String isbn;
     private String description;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
 }
