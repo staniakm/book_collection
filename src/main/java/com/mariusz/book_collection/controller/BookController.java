20192c38d62bb702b3a0036937a1f1adc1ba0068
Mariusz <mariusz.staniak@gmail.com>
2018-09-01 09:27:00 +0200

    add controller for books

---
+++ src/main/java/com/mariusz/book_collection/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.mariusz.book_collection.controller;
+
+import com.mariusz.book_collection.entity.Book;
+import com.mariusz.book_collection.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/books")
+public class BookController {
+
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping(value = "", produces = {"application/hal+json"})
+    public ResponseEntity<Book> create(@RequestBody final Book book) {
+        Book createdBook = bookService.saveOrUpdate(book);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+    }
+}
