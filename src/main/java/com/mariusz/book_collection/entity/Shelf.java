289bc759d2b58ebe93053f0f7505bee846ce85d2
Mariusz <mariusz.staniak@gmail.com>
2018-09-01 13:33:06 +0200

    add Shelf class

---
+++ src/main/java/com/mariusz/book_collection/entity/Shelf.java
@@ -0,0 +1,24 @@
+package com.mariusz.book_collection.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Shelf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+
+}
