e0c8debbe097eb27f1a88b199580aef5ae76eb7e
Mariusz <mariusz.staniak@gmail.com>
2018-09-22 10:41:53 +0200

    add integration tests

--- src/test/java/com/mariusz/book_collection/integration/IntegrationTests.java
+++ src/test/java/com/mariusz/book_collection/integration/IntegrationTests.java
@@ -1,8 +1,9 @@
 package com.mariusz.book_collection.integration;
 
 import com.mariusz.book_collection.entity.Book;
+import com.mariusz.book_collection.entity.Shelf;
 import com.mariusz.book_collection.repository.BookRepository;
-import org.assertj.core.api.Assertions;
+import com.mariusz.book_collection.repository.ShelfRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +15,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,25 +28,71 @@ public class IntegrationTests {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private ShelfRepository shelfRepository;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         Book book = new Book();
         book.setId(1L);
         book.setTitle("Pinokio");
         book.setAuthor("Carl Collodi");
         bookRepository.save(book);
+
+        Shelf shelf = new Shelf();
+        shelf.setDescription("Bedroom");
+        shelfRepository.save(shelf);
+
+    }
+
+    @After
+    public void cleanUp(){
+        bookRepository.flush();
+        shelfRepository.flush();
     }
 
 
     @Test
-    public void getBookById_willReturnBook(){
+    public void getBookById_willReturnBook() {
 
         ResponseEntity<Book> response = restTemplate.getForEntity("/api/books/1", Book.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody().getTitle()).isEqualToIgnoringCase("Pinokio");
         assertThat(response.getBody().getAuthor()).isEqualToIgnoringCase("Carl Collodi");
-
-
     }
+
+    @Test
+    public void getBookById_willgetStatusNOtFound(){
+        ResponseEntity<Book> response = restTemplate.getForEntity("/api/books/22",Book.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void getShelfById_willReturnShelf(){
+        ResponseEntity<Shelf> response = restTemplate.getForEntity("/api/shelfs/1", Shelf.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getDescription()).isEqualToIgnoringCase("Bedroom");
+    }
+
+    @Test
+    public void getShelfById_willReturnNotFoundStatus(){
+        ResponseEntity<Shelf> response = restTemplate.getForEntity("/api/shelfs/22", Shelf.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void putBook_willReturnBookWithId(){
+        Book book = new Book();
+        book.setTitle("Pankracy");
+
+        Book newBook = restTemplate.postForObject("/api/books", book, Book.class);
+
+        assertThat(newBook).isNotNull();
+        assertThat(newBook.getId()).isNotZero();
+        assertThat(newBook.getTitle()).isEqualToIgnoringCase("Pankracy");
+    }
+
 }
